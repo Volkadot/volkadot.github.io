0af2b585e7a8ae3a03255466a59b47318b2d4cb7
AozoraDev <aozoradeveloper@gmail.com>
2024-04-30 12:19:23 +0800

    Increase dots size

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function drawCircles(canvas) {
-    const circleColor = "rgba(255, 255, 255, 0.3)";
-    const circleSize = 2;
+    const circleColor = "rgba(255, 255, 255, 0.2)";
+    const circleSize = 5;
     const circleSpacing = 20;
 
     const ctx = canvas.getContext("2d");
